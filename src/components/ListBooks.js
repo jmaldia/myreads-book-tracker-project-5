6390c91f425fd3d073cdd1eebe585c64801a9af9
Jon Maldia <jon@maldia.net>
2018-10-05 00:08:48 -0400

    Fix issue with search

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -10,41 +10,38 @@ class ListBooks extends Component {
                         {
                             this.props.books.map((book) => {
                                 return (
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div 
-                                                        className="book-cover" 
-                                                        style={{ 
-                                                            width: 128, 
-                                                            height: 193, 
-                                                            backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})` 
-                                                        }}
-                                                    >
-                                                    </div>
-                                                    <div className="book-shelf-changer">
-                                                        <select 
-                                                            value={book.shelf} 
-                                                            onChange={
-                                                                (event) => {
-                                                                    this.props.handleChange(book, event.target.value)
-                                                            }}
-                                                        >
-                                                            <option value="move" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                        </select>
-                                                    </div>
+                                    <li key={book.id}>
+                                        <div className="book">
+                                            <div className="book-top">
+                                                <div 
+                                                    className="book-cover" 
+                                                    style={{ 
+                                                        width: 128, 
+                                                        height: 193, 
+                                                        backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})` 
+                                                    }}
+                                                >
                                                 </div>
-                                                <div className="book-title">{book.title}</div>
-                                                <div className="book-authors">{book.authors}</div>
+                                                <div className="book-shelf-changer">
+                                                    <select 
+                                                        value={book.shelf} 
+                                                        onChange={(event) => this.props.handleChange(book, event.target.value)}
+                                                    >
+                                                        <option value="move" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                    </select>
+                                                    
+                                                </div>
                                             </div>
-                                        </li>
-                                    )
-                                }
-                            )
+                                            <div className="book-title">{book.title}</div>
+                                            <div className="book-authors">{book.authors}</div>
+                                        </div>
+                                    </li>
+                                )
+                            })
                         }
                     </ol> 
                 </div>   
--- src/views/Search.js
+++ src/views/Search.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import * as BooksAPI from '../BooksAPI'
 import ListBooks from '../components/ListBooks'
 
-let tempBooks = []
+// let tempBooks = []
 
 class Search extends Component {
     state = {
@@ -13,11 +13,6 @@ class Search extends Component {
     }
   
     componentDidMount() {
-        // this.search(this.state.keyword)
-        // BooksAPI.getAll()
-        //     .then( books => { 
-        //         this.setState({ allBooks : books })
-        //     })
     }
   
     handleChange = (book, value) => {
@@ -28,7 +23,7 @@ class Search extends Component {
   
     handleKeyword = (keyword) => {
         this.setState({ keyword }, this.search(keyword))
-        tempBooks = []
+        // tempBooks = []
     }
 
     search = (keyword) => {
@@ -38,27 +33,23 @@ class Search extends Component {
             return this.setState({ searchResults: [] })
         } else {
             BooksAPI.search(keyword)
-                .then(searchedBooks => {
-                    if(searchedBooks.error) {
+                .then(results => {
+                    if(results.error) {
                         return this.setState({ searchResults: [] })
                     }
-                    
-                    // this.state.allBooks.forEach(book => {
-                    //     searchedBooks.forEach(searchedBook => {
-                    //         if (searchedBook.id === book.id) {
-                    //             tempBooks.push(book)
-                    //         }
-                    //     })
-                    // })
 
-                    searchedBooks.forEach(searchedBook => {
-                        BooksAPI.get(searchedBook.id).then((book) => {
-                            console.log(book);
-                            return tempBooks.push(book)
+                    let temp = results.map(book => {
+                        BooksAPI.get(book.id).then((bookWithID) => {
+                            book.shelf = bookWithID.shelf
                         })
+                        return book
                     })
 
-                    return this.setState({ searchResults: tempBooks })
+                    return this.setState({
+                        searchResults: temp
+                    })
+
+                    // return this.setState({ searchResults })
                 })
                 .catch(err => {
                     return this.setState({ searchResults: [] })
@@ -77,14 +68,6 @@ class Search extends Component {
                     Closed
                 </Link>
                 <div className="search-books-input-wrapper">
-                    {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                    */}
                     <input type="text" placeholder="Search by title or author" onChange={(event) => this.handleKeyword(event.target.value)}/>
                 </div>
                 </div>
